55fe17f071e3b1cc2a6534e83f3eb0e6d0926a14
maxim oberemchenko <50733191+omg1980@users.noreply.github.com>
2021-06-01 22:13:34 +0300

    images list

--- lib/ios.dart
+++ lib/ios.dart
@@ -13,21 +13,16 @@ class IosIconTemplate {
 }
 
 List<IosIconTemplate> iosIcons = <IosIconTemplate>[
-  IosIconTemplate(name: '-20x20@1x', size: 20),
-  IosIconTemplate(name: '-20x20@2x', size: 40),
-  IosIconTemplate(name: '-20x20@3x', size: 60),
-  IosIconTemplate(name: '-29x29@1x', size: 29),
-  IosIconTemplate(name: '-29x29@2x', size: 58),
-  IosIconTemplate(name: '-29x29@3x', size: 87),
-  IosIconTemplate(name: '-40x40@1x', size: 40),
-  IosIconTemplate(name: '-40x40@2x', size: 80),
-  IosIconTemplate(name: '-40x40@3x', size: 120),
-  IosIconTemplate(name: '-60x60@2x', size: 120),
-  IosIconTemplate(name: '-60x60@3x', size: 180),
-  IosIconTemplate(name: '-76x76@1x', size: 76),
-  IosIconTemplate(name: '-76x76@2x', size: 152),
-  IosIconTemplate(name: '-83.5x83.5@2x', size: 167),
-  IosIconTemplate(name: '-1024x1024@1x', size: 1024),
+  IosIconTemplate(name: '-20', size: 20),
+  IosIconTemplate(name: '-60', size: 60),
+  IosIconTemplate(name: '-29', size: 29),
+  IosIconTemplate(name: '-58', size: 58),
+  IosIconTemplate(name: '-87', size: 87),
+  IosIconTemplate(name: '-40', size: 40),
+  IosIconTemplate(name: '-80', size: 80),
+  IosIconTemplate(name: '-120', size: 120),
+  IosIconTemplate(name: '-180', size: 180),
+  IosIconTemplate(name: '-1024', size: 1024),
 ];
 
 void createIcons(Map<String, dynamic> config, String? flavor) {
